0a0468f573f1f3541d2732c7a637e72ff4e19e63
MickeyJ <mickeyjmalotte@gmail.com>
2025-05-26 23:12:37 -0700

    add debug step to deploy workflow

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,3 +2,8 @@
 output "app_runner_url" {
   value = "https://${aws_apprunner_service.food_data_api.service_url}"
 }
+
+# Output the role ARN for debugging
+output "github_actions_role_arn" {
+  value = aws_iam_role.github_actions.arn
+}
